c5fe688b90d31f483e060abab4782cbe5ba285b6
gksriharsha <51872746+gksriharsha@users.noreply.github.com>
2019-11-26 17:56:49 +0530

    paths field added and classify button greyed
    Path files should be kept next to button (preferred). Brains class is also created

--- Class_GUI.py
+++ Class_GUI.py
@@ -1,5 +1,7 @@
 from tkinter import *
 from tkinter import filedialog
+import json
+import Brains
 
 root = Tk()
 root.title("Classification Selection Tool")
@@ -17,15 +19,21 @@ fileoptn = StringVar()
 fileoptn.set("Two files")
 
 global paths
-paths = []
+paths = [' ']*2
+
+global settings
+global auto_generated_settings
+global user_settings
+
+settings = {}
+user_settings = {}
+auto_generated_settings = {}
+
 
 def classifier_selection():
-    classifierframe = LabelFrame(root)
+    classifierframe = LabelFrame(root,text="Choose the classifier",pady=10)
     classifierframe.config(height=height_widget,width=width_widget)
-    classifierframe.pack_propagate(0)
-
-    classificationlabel = Label(classifierframe,text="Choose the classifier required")
-    classificationlabel.pack()
+    
 
     classifiers = [
         ("SVM Classifier","SVM"),
@@ -36,42 +44,67 @@ def classifier_selection():
     for classifier,value in classifiers:
         Radiobutton(classifierframe,text=classifier, variable=selectedclassifier,value=value).pack(anchor=W)
 
+    classifierframe.pack_propagate(0)
     classifierframe.pack(pady=10)
 
    
 
-def data_openfile():
-    root.filename = filedialog.askopenfilename(initialdir="/", title="Select Data for Classification", filetypes=(("csv files", "*.csv"),("all files", "*.*")))   
-    paths.append(root.filename)
-    Label(root, text=root.filename).pack()
-
-def label_openfile():
-    root.filename = filedialog.askopenfilename(initialdir="/", title="Select Labels for Classification", filetypes=(("csv files", "*.csv"),("all files", "*.*")))   
-    paths.append(root.filename)
-    Label(root, text=root.filename).pack()
 
         
 def file_selection(): 
+
+    fileframe = LabelFrame(root,text="Classifier Data Setup",pady=10)
+    fileframe.config(height=300,width=width_widget)
 
     def file_number_choose(fileoptn):   
         if(fileoptn.get() == "One file"):
             data_button.config(text="Open Data + Labels file")        
             label_button.config(state="disabled")
-            paths.clear()   
+            file_path_labels.set("")
+            file_path_data.set("")
+            paths = [' ']*2               
         else:
             data_button.config(text="Open Data file")
             label_button.config(state="normal")
-            paths.clear()        
+            file_path_labels.set("")
+            file_path_data.set("")
+            paths = [' ']*2                   
+    file_path_data = StringVar()
+    file_path_labels = StringVar()
+    data_path = Entry(fileframe,state='readonly',relief=SUNKEN,width=80,textvariable=file_path_data)
+    label_path = Entry(fileframe,state='readonly',relief=SUNKEN,width=80,textvariable=file_path_labels)
 
-    fileframe = LabelFrame(root)
-    fileframe.config(height=height_widget,width=width_widget)
-    
+    def data_openfile():
+        root.filename = filedialog.askopenfilename(initialdir="/", title="Select Data for Classification", filetypes=(("csv files", "*.csv"),("all files", "*.*")))   
+        paths[0] = root.filename        
+        if(paths):            
+            data_path.delete(0,END)
+            data_path.insert(0,str(paths[0]))
+            file_path_data.set(paths[0])
+            
+        Label(root, text=root.filename).pack()
+
+    def label_openfile():
+        root.filename = filedialog.askopenfilename(initialdir="/", title="Select Labels for Classification", filetypes=(("csv files", "*.csv"),("all files", "*.*")))   
+        paths[1] = root.filename
+        if(paths):
+            label_path.delete(0,END)
+            label_path.insert(0,str(paths[1]))
+            file_path_labels.set(paths[1])
+            
+        Label(root, text=root.filename).pack()
+
     Radiobutton(fileframe,text="Data and Labels in single file",variable=fileoptn,value="One file",command=lambda :file_number_choose(fileoptn)).pack(anchor=W)
     Radiobutton(fileframe,text="Data and Labels as seperate files",variable = fileoptn,value="Two files",command=lambda :file_number_choose(fileoptn)).pack(anchor=W)
+
     data_button = Button(fileframe,text="Open Data file",command=data_openfile)
     label_button = Button(fileframe,text="Open Labels file",command=label_openfile)
     data_button.pack(padx=5,pady=10,anchor=W)
+    data_path.pack(padx=5,pady=10,anchor=W)
     label_button.pack(padx=5,pady=10,anchor=W)
+    label_path.pack(padx=5,pady=10,anchor=W)
+
+   
 
     fileframe.pack_propagate(0)
     fileframe.pack()
@@ -81,12 +114,31 @@ def file_selection():
 def click_button():
     def clicked(value1,value2):
         myLabel = Label(root, text=value1+" "+value2)
-        myLabel.pack()	
-
-    myButton = Button(root, text="Click Me!", command=lambda: clicked(selectedclassifier.get(),fileoptn.get()))
+        myLabel.pack()
+        finalize()	
+    myButton = Button(root, text="Classify", state=DISABLED,command=lambda: clicked(selectedclassifier.get(),fileoptn.get()))
+    
+    if((len(paths[1]) >= 10 and fileoptn.get() == "Two files") or (len(paths[0]) >= 10 and fileoptn.get() == "One file")):
+        myButton.config(state=NORMAL)    
     myButton.pack(pady=10)
+
+def write_settings():    
+    user_settings['Number of files'] = fileoptn.get()
+    user_settings['Classifier'] = selectedclassifier.get()
+    auto_generated_settings['Features'] = Brains.get_number_of_cols(paths[0])
+    settings['User selected settings'] = user_settings
+def finalize():
+    
+    f = open("settings.json",'w+')
+    print(json.dumps(settings,indent=4)) 
+    f.write(json.dumps(settings,indent=4))
+    f.close()
+    write_settings()
+
 
 classifier_selection()
 file_selection()
 click_button()
+
+
 root.mainloop()
