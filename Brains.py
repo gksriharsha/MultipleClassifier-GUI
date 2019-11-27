50a6777695b40fa657679ab384a4a919b169b481
Gundu <806707@cognizant.com>
2019-11-27 17:01:46 +0530

    Major parts completed. Final touches pending

--- Brains.py
+++ Brains.py
@@ -1,18 +1,33 @@
 import pandas as pd
 import numpy as np
+from sklearn.metrics import accuracy_score
+import json
 
 raw_file = []
-
+data_test = []
+data_train = []
+label_test = []
+label_train = []
+labels = []
+classifier_results = {}
+rows = 0
+cols = 0
+classes = 0
 def get_number_of_cols(path):
+    global cols
     file = pd.read_csv(path,nrows=1)
-    return len(file)
+    cols = len(file)
+    return cols
 
 def get_number_of_rows(path):
+    global rows
     file = pd.read_csv(path)
-    return (file.shape)[1]
+    rows = file.shape[1]
+    return rows
 
 def get_number_of_classes(path):
     cols = get_number_of_cols(path)
+    global classes
     file = pd.read_csv(path)   
     classes = len(np.unique(file.iloc[:,cols-1]))
     return classes
@@ -28,9 +43,105 @@ def get_number_of_classes(path):
     temp_list.sort()
     return unique_list.index(temp_list[0:5]) """
 
-def classify(path,user_settings,classifier_settings):
-    global raw_file
-    raw_file = pd.read_csv(path)
+def classify(paths,user_settings,classifier_settings):
+    from sklearn.model_selection import train_test_split 
+    global raw_file,label_test,label_train,data_test,data_train,cols,labels  
+    cols = get_number_of_cols(paths[0])
+    if(user_settings["Headers"] == "Yes"):
+        raw_file = pd.read_csv(paths[0],skiprows=1)
+        if(user_settings['Number of files'] == "One file"):            
+            dfs = np.split(raw_file,[cols-2],axis=1)
+            data = dfs[0]
+            labels = dfs[1]            
+        else:
+            labels = pd.read_csv(paths[1],skiprows=1)
+    else:
+        raw_file = pd.read_csv(paths[0])
+        if(user_settings['Number of files'] == "One file"):            
+            dfs = np.split(raw_file,[cols-2],axis=1)
+            data = dfs[0]
+            labels = dfs[1]
+        else:
+            labels = pd.read_csv(paths[1])            
+    
+    data_train, data_test, label_train, label_test = train_test_split(data, labels, test_size=0.33, random_state=42)
+    if(user_settings['Classifier'] == "KNN"):
+        classify_KNN(classifier_settings)
+    elif(user_settings['Classifier'] == "SVM"):
+        classify_SVM(classifier_settings)
+    elif(user_settings['Classifier'] == "MLP"):
+        classify_MLP(classifier_settings)
+    else:
+        auto_classify(paths)
+    print("End of classification")
+    write_results()
+
 def classify_KNN(classifier_settings):
-    from sklearn.neighbors import KNeighborsClassifier
-    clf_KNN = KNeighborsClassifier(n_neighbors=classifier_settings["K"], weights=classifier_settings["weights"])
+    global classifier_results,data_test,data_train,label_test,label_train
+    from sklearn.neighbors import KNeighborsClassifier    
+    if(bool(classifier_settings)):
+        clf_KNN = KNeighborsClassifier(n_neighbors=classifier_settings['K'], weights=classifier_settings['weights'])
+    else:
+        clf_KNN = KNeighborsClassifier(n_neighbors=3)
+    clf_KNN.fit(data_train,label_train)
+    pred = clf_KNN.predict(data_test)    
+    acc = accuracy_score(label_test,pred)*100
+    classifier_results["Accuracy"] = acc
+    print(acc)
+    #return classifier_results
+
+def classify_SVM(classifier_settings):
+    global classifier_results,data_test,data_train,label_test,label_train
+    from sklearn.svm import SVC    
+    if(bool(classifier_settings)):
+        clf_SVM = SVC(degree=int(classifier_settings["Degree"]),tol=float(classifier_settings["tol"]))
+    else:
+        clf_SVM = SVC()
+    clf_SVM.fit(data_train,label_train)
+    pred = clf_SVM.predict(data_test)
+    acc = accuracy_score(label_test,pred)*100
+    classifier_results["Accuracy"] = acc
+    print(acc)
+    #return classifier_results
+
+def classify_MLP(classifier_settings):
+    global classifier_results,data_test,data_train,label_test,label_train    
+    from sklearn.neural_network import MLPClassifier
+    if(bool(classifier_settings)):
+        clf_MLP =  MLPClassifier(solver='lbfgs',hidden_layer_sizes=(5, 2),
+                activation=classifier_settings['Activation Func.'],max_iter=int(classifier_settings['Iterations']),tol=float(classifier_settings['Tolerance']))
+    else:
+        clf_MLP =  MLPClassifier()
+    clf_MLP.fit(data_train,label_train)
+    pred = clf_MLP.predict(data_test)
+    acc = accuracy_score(label_test,pred)*100
+    classifier_results["Accuracy"] = acc
+    print(acc)
+    #return classifier_results
+
+def auto_classify(paths):
+    global classes,cols,rows,classifier_results
+    classifier_settings = {}
+    if(classes == 0):
+        classes = len(np.unique(labels))
+    if(cols == 0):
+        cols = get_number_of_cols(paths[1] if len(paths[1])>5 else paths[0])
+    if(rows == 0):
+        rows = len(labels)
+    
+    NGI = rows/(classes*cols)
+
+    if(NGI<1):
+        classify_MLP(classifier_settings)
+        classifier_results["selected classifier"] = "MLP"
+    elif(NGI>1 and NGI <10):
+        classify_SVM(classifier_settings)
+        classifier_results["selected classifier"] = "SVM"
+    else:
+        classify_KNN(classifier_settings)
+        classifier_results["selected classifier"] = "KNN"
+
+def write_results():
+    f=open("results.json","w+")
+    f.write(json.dumps(classifier_results,indent=4))
+    f.close()
--- additional_settings.py
+++ additional_settings.py
@@ -1,8 +1,9 @@
 from tkinter import *
 import json
-
-global classifier_settings
-classifier_settings = {}
+try:
+    classifier_settings
+except:
+    classifier_settings = {} 
 def isint(number):
     try:
         if(number is None or number is ""):
@@ -24,12 +25,13 @@ def SVM(settings_window):
     degree_SVM = IntVar()
     degree_SVM.set(3)
     tol_SVM = DoubleVar()
-    tol_SVM.set(0.001)
+    tol_SVM.set(0.0001)
     reg = settings_window.register(isint)
     reg2 = settings_window.register(isfloat)    
     def SVM_close():
+        global classifier_settings
         classifier_settings = {}
-        classifier_settings["Degree "] = degree_SVM.get()        
+        classifier_settings["Degree"] = degree_SVM.get()        
         classifier_settings["tol"] = tol_SVM.get()
         print(json.dumps(classifier_settings,indent=4))
         settings_window.destroy()
@@ -41,21 +43,23 @@ def SVM(settings_window):
 
 def KNN(settings_window):
     K_KNN = IntVar()
+    K_KNN.set(classifier_settings['K'])
     fn = StringVar()
     reg = settings_window.register(isint)
     reg2 = settings_window.register(isfloat)    
     def KNN_close():
+        global classifier_settings
         classifier_settings = {}
-        classifier_settings["K"] = K_KNN.get()
-        classifier_settings["weights"] = fn.get()
+        classifier_settings['K'] = K_KNN.get()
+        classifier_settings['weights'] = fn.get()
         print(json.dumps(classifier_settings,indent=4))
         settings_window.destroy()
     Label(settings_window,text="N neighbors:    ").grid(row=0,column=0)
     Entry(settings_window,textvariable=K_KNN,validate="key",vcmd=(reg,'%P')).grid(row=0,column=1,pady=10)
     Label(settings_window,text="Weights :    ").grid(row=1,column=0)
     weights =  [
-                    "Uniform", 
-                    "Inv. distance"                                           
+                    "uniform", 
+                    "distance"                                           
                 ]            
     fn.set(weights[0])
     OptionMenu(settings_window, fn, *weights).grid(row=1,column=1,pady=10)
@@ -64,24 +68,27 @@ def KNN(settings_window):
 def MLP(settings_window):
     fn = StringVar()
     Iter = IntVar()
+    Iter.set(int(classifier_settings["Iterations"]))
     tol_MLP = DoubleVar()
+    tol_MLP.set(float(classifier_settings["Tolerance"]))
     reg = settings_window.register(isint)
     reg2 = settings_window.register(isfloat)    
     def MLP_close():
+        global classifier_settings
         classifier_settings = {}
-        classifier_settings["fn"] = fn.get()
+        classifier_settings["Activation Func."] = fn.get()
         classifier_settings["Iterations"] = Iter.get()
         classifier_settings["Tolerance"] = tol_MLP.get()
         print(json.dumps(classifier_settings,indent=4))
         settings_window.destroy()
     Label(settings_window,text="Activation func. :    ").grid(row=0,column=0)
     activation =  [
-                    "Relu", 
-                    "Tanh", 
-                    "Logistic", 
-                    "Identity"                           
+                    "relu", 
+                    "tanh", 
+                    "logistic", 
+                    "identity"                           
                 ]            
-    fn.set(activation[0])
+    fn.set(classifier_settings['Activation Func.'])
     OptionMenu(settings_window, fn, *activation).grid(row=0,column=1,pady=10)
     Label(settings_window,text="Max Iterations:    ").grid(row=1,column=0,pady=10)
     Entry(settings_window,textvariable=Iter,validate="key",vcmd=(reg,'%P')).grid(row=1,column=1,pady=10)
@@ -90,19 +97,21 @@ def MLP(settings_window):
     Button(settings_window,text="Save and close",command=MLP_close).grid(row=3,column=0,columnspan=2,pady=10)
 
 def additional_settings(value):
-        settings_window = Toplevel()
-        settings_window.grab_set()
-        #settings_window.geometry("250x150")
-        #settings_window.minsize(150,150)
-        settings_window.title('Classifier Settings')
-            
-        if(value == "SVM"):
-            SVM(settings_window)
-        elif(value == "KNN"):
-            KNN(settings_window)
-        elif(value == "MLP"):
-            MLP(settings_window)
-        else:
-            pass
+    global classifier_settings
+    with open('settings.json') as f:
+        classifier_settings = json.load(f)
+    settings_window = Toplevel(padx=10,pady=10)
+    settings_window.grab_set()
+    settings_window.resizable(False,False)       
+    settings_window.title('Classifier Settings')
+        
+    if(value == "SVM"):
+        SVM(settings_window)
+    elif(value == "KNN"):
+        KNN(settings_window)
+    elif(value == "MLP"):
+        MLP(settings_window)
+    else:
+        pass
         
     
