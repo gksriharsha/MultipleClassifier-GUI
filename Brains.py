778cd5a27e23144643eaa161108efa58d9b3ac26
Gundu <806707@cognizant.com>
2020-01-28 11:16:24 +0530

    Code Cleanup

--- Brains.py
+++ Brains.py
@@ -33,16 +33,6 @@ def get_number_of_classes(path):
     classes = len(np.unique(file.iloc[:,cols-1]))
     return classes
 
-""" def search_classes(path):
-    cols = get_number_of_cols(path)
-    file = pd.read_csv(path)
-    unique_list = [0] * cols
-    for ind in reversed(range(cols)):
-        column = file.iloc[:,ind]
-        unique_list[ind] = len(np.unique(column)) 
-    temp_list = unique_list
-    temp_list.sort()
-    return unique_list.index(temp_list[0:5]) """
 def big_data_classify():
     print("File size too large - Module under implementation")
 
